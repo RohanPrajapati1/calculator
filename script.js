e2b47f704e80f685eb14415fdd570f335e040117
RohanPrajapati1 <dp.kumarohanpr9528@gmail>
2024-11-02 14:48:10 +0530

    add javascript

---
+++ script.js
@@ -0,0 +1,23 @@
+const display = document.getElementById("input");
+console.log(display);
+let buttons = document.querySelectorAll(".button");
+console.log(buttons);
+
+let string = "";
+let arr = Array.from(buttons);
+
+arr.forEach(button =>{
+    button.addEventListener('click' , (e) => {
+        if(e.target.innerHTML == '='){
+            string = eval(string);
+            display.innerHTML = string;
+        }else if(e.target.innerHTML == 'AC'){
+            display.innerHTML = '0';
+        }
+
+        else{
+            string += e.target.innerHTML;
+            display.innerHTML = string;
+        }
+    })
+})
